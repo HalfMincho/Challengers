89daf6a2a2af6d61f4f74d10d1f60fa2b286ff57
GlassK <glassk0724@gmail.com>
2022-01-11 15:25:19 +0900

    feat: 챌린지 사진 업로드 컴포넌트

--- frontend/src/components/ChallengeInfo.js
+++ frontend/src/components/ChallengeInfo.js
@@ -1,41 +1,104 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import {
+  Box,
   Card,
   CardActions,
   CardContent,
   CardMedia,
+  Divider,
   IconButton,
   Stack,
+  TextField,
   Typography,
 } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../styles/theme';
 import DefaultButton from './DefaultButton';
-import { MonetizationOn, ShoppingCartOutlined, BookmarkBorder } from '@mui/icons-material';
+import {
+  MonetizationOn,
+  ShoppingCartOutlined,
+  BookmarkBorder,
+  PhotoCamera,
+} from '@mui/icons-material';
+
+const Input = styled('input')({
+  display: 'none',
+});
+
+const showRightSideButtons = () => {
+  return (
+    <CardActions>
+      <Stack alignItems="center" direction="row" spacing={1}>
+        <IconButton color="primary" size="large">
+          <BookmarkBorder fontSize="inherit" padding="3px" />
+        </IconButton>
+        <IconButton color="primary" size="large">
+          <ShoppingCartOutlined fontSize="inherit" padding="3px" />
+        </IconButton>
+        <DefaultButton color="neutral" size="small" text="문의하기" variant="outlined" />
+      </Stack>
+    </CardActions>
+  );
+};
+
+const submitPhoto = () => {
+  return (
+    <CardContent sx={{ padding: '30px' }}>
+      <Divider variant="middle" />
+      <Typography mt={5} sx={{ fontWeight: 500, fontSize: '1.2em' }}>
+        인증샷 업로드
+      </Typography>
+      <Stack
+        alignItems="center"
+        direction="row"
+        justifyContent="center"
+        margin="15px"
+        spacing={0.5}
+      >
+        <Box>
+          <label htmlFor="icon-button-file">
+            <Input accept="image/*" id="icon-button-file" type="file" />
+            <IconButton aria-label="upload picture" color="primary" component="span">
+              <PhotoCamera />
+            </IconButton>
+          </label>
+        </Box>
+        <Box
+          autoComplete="off"
+          component="form"
+          noValidate
+          sx={{
+            '& > :not(style)': { m: 1, width: '55em' },
+          }}
+        >
+          <TextField
+            color="neutral"
+            fullWidth
+            id="outlined-multiline-static"
+            label="이번 인증은 어떠셨나요?"
+            multiline
+            rows={4}
+          />
+        </Box>
+      </Stack>
+    </CardContent>
+  );
+};
 
 const ChallengeInfo = ({ data }) => {
   return (
     <ThemeProvider theme={theme}>
       <Card sx={{ minWidth: 275, marginTop: 5 }} variant="outlined">
         {data.image && (
-          <CardMedia alt="챌린지 대표 이미지" component="img" height="140" image={data.image} />
+          <CardMedia alt="챌린지 대표 이미지" component="img" height="130" image={data.image} />
         )}
-        <CardContent>
-          <Stack align-items="flex-end" direction="row" justifyContent="space-between">
+        <CardContent sx={{ padding: '30px' }}>
+          <Stack alignItems="flex-end" direction="row" justifyContent="space-between">
             <Typography color="text.secondary" sx={{ fontSize: 14 }}>
               {data.start_at}부터 시작
             </Typography>
-            <CardActions>
-              <Stack direction="row" spacing={1}>
-                <IconButton color="primary" size="large">
-                  <BookmarkBorder fontSize="inherit" padding="3px" />
-                </IconButton>
-                <IconButton color="primary" size="large">
-                  <ShoppingCartOutlined fontSize="inherit" padding="3px" />
-                </IconButton>
-                <DefaultButton color="neutral" size="small" text="문의하기" variant="outlined" />
-              </Stack>
-            </CardActions>
+            {showRightSideButtons()}
           </Stack>
           <Typography sx={{ fontWeight: 500, fontSize: '1.5em' }}>{data.name}</Typography>
           <Stack alignItems="center" direction="row" spacing={1}>
@@ -57,11 +120,14 @@ const ChallengeInfo = ({ data }) => {
             </Stack>
             <Typography sx={{ fontSize: '0.9em' }}>총 {data.cost}원</Typography>
           </Stack>
+        </CardContent>
+        <CardContent sx={{ padding: '30px' }}>
           <Typography mt={2} sx={{ fontWeight: 500, fontSize: '1.2em' }}>
             인증 방법
           </Typography>
           <Typography>{data.description}</Typography>
         </CardContent>
+        {submitPhoto()}
       </Card>
     </ThemeProvider>
   );
