4d41a0b12cf2a0b8e6abb8012d5ae1e61093b86f
GlassK <glassk0724@gmail.com>
2022-01-15 17:38:32 +0900

    fix: MUI 환경설정 에러

--- frontend/src/components/ChallengeInfo.js
+++ frontend/src/components/ChallengeInfo.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import { ShoppingCartOutlined, BookmarkBorder } from '@mui/icons-material';
-import theme from '../styles/theme';
 import styled from 'styled-components';
 import DefaultButton from './DefaultButton';
 import Tag from './Tag';
@@ -30,35 +26,27 @@ const InfoContainer = styled.div`
 
 const ChallengeInfo = ({ data }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <InfoContainer>
-        <div className="buttons">
-          <IconButton color="primary" size="large">
-            <BookmarkBorder fontSize="inherit" padding="3px" />
-          </IconButton>
-          <IconButton color="primary" size="large">
-            <ShoppingCartOutlined fontSize="inherit" padding="3px" />
-          </IconButton>
-          <DefaultButton color="neutral" size="small" text="문의하기" variant="outlined" />
+    <InfoContainer>
+      <div className="buttons">
+        <DefaultButton color="neutral" size="small" text="문의하기" variant="outlined" />
+      </div>
+      <section>
+        <p>{data.start_at}부터 시작</p>
+        <h2>{data.name}</h2>
+        <div className="dateInfo">
+          <Tag text={data.auth_day} />
+          <Tag text={data.auth_count_in_day} />
+          <span>
+            {data.start_at}~{data.end_at}
+          </span>
         </div>
-        <section>
-          <p>{data.start_at}부터 시작</p>
-          <h2>{data.name}</h2>
-          <div className="dateInfo">
-            <Tag text={data.auth_day} />
-            <Tag text={data.auth_count_in_day} />
-            <span>
-              {data.start_at}~{data.end_at}
-            </span>
-          </div>
-          <p>참가비 {data.cost}원</p>
-        </section>
-        <section>
-          <h3>인증 방법</h3>
-          <p>{data.description}</p>
-        </section>
-      </InfoContainer>
-    </ThemeProvider>
+        <p>참가비 {data.cost}원</p>
+      </section>
+      <section>
+        <h3>인증 방법</h3>
+        <p>{data.description}</p>
+      </section>
+    </InfoContainer>
   );
 };
 
