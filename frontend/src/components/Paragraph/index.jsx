8012c20cfe5e07af56911f598737588e889281d7
YuriKim <glassk0724@gmail.com>
2022-02-01 11:38:40 +0900

    Button 기본값 할당, Paragraph 스타일 수정

--- frontend/src/components/Paragraph/index.jsx
+++ frontend/src/components/Paragraph/index.jsx
@@ -2,7 +2,7 @@ import './style.scss';
 
 export default function Paragraph({ children }) {
   return (
-    <section>
+    <section className="Paragraph">
       <p className="title">{children[0]}</p>
       <div className="content">{children[1]}</div>
     </section>
