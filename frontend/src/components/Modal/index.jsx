883d2791a5072e833168fa75fd374062186262fe
YuriKim <glassk0724@gmail.com>
2022-02-07 15:30:16 +0900

    Modal 공통 컴포넌트

---
+++ frontend/src/components/Modal/index.jsx
@@ -0,0 +1,25 @@
+import { GrClose } from 'react-icons/gr';
+import './style.scss';
+
+export default function Modal({ title, subTitle, middleContent, visible, onClose }) {
+  const closeModal = (e) => {
+    if (onClose) onClose(e);
+  };
+
+  return (
+    <>
+      <div className={`modalOverlay ${visible ? 'visible' : 'none'}`} onClick={closeModal}></div>
+      <div
+        className={`modalWrapper ${visible ? 'visible' : 'none'}`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="closeIcon">
+          <GrClose onClick={closeModal} />
+        </div>
+        {title && <p className="title">{title}</p>}
+        {subTitle && <p className="subTitle">{subTitle}</p>}
+        {middleContent}
+      </div>
+    </>
+  );
+}
