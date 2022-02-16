4afb1705283309f7aab7876dc38a6c3e5bbe741d
GanghyeonSeo <sgh000109@gmail.com>
2022-02-16 21:58:05 +0900

    wrapper 함수 추가

--- src/api/account.ts
+++ src/api/account.ts
@@ -10,72 +10,73 @@ import {
   ModifyPassword,
 } from "../services/account";
 import { AuthJWT } from "../services/middlewares/auth";
+import { wrap } from "../services/utils/wrapper";
 
 const accountRouter = express.Router();
 
 accountRouter.post(
   "/register-token",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await GenerateRegisterToken(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 accountRouter.post(
   "/verify-token",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await VerifyRegisterToken(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 accountRouter.post(
   "/register",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await Register(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 accountRouter.post(
   "/login",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await Login(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 accountRouter.get(
   "/refresh",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await TokenRefresh(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 accountRouter.put(
   "/user/name",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await ModifyUsername(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 accountRouter.put(
   "/user/password",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await ModifyPassword(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 export default accountRouter;
--- src/api/challenge.ts
+++ src/api/challenge.ts
@@ -19,12 +19,13 @@ import {
 import { Category } from "../types/challenge";
 
 import { AuthJWT } from "../services/middlewares/auth";
+import { wrap } from "../services/utils/wrapper";
 
 const challengeRouter = express.Router();
 
 challengeRouter.get(
   "/search",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     if ("keyword" in req.query && !("category" in req.query)) {
       const { status, result } = await GetChallengeWithTitle(
         String(req.query.keyword),
@@ -40,128 +41,128 @@ challengeRouter.get(
       res.status(status);
       res.send(result);
     }
-  },
+  }),
 );
 
 challengeRouter.get(
   "/popular",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await GetPopularChallenge(
       Number(req.query.count),
     );
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.get(
   "/recent",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await GetRecentChallenge(
       Number(req.query.count),
     );
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.get(
   "/open",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await GetOpenChallenge(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.get(
   "/participate",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await GetParticipateChallenge(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.get(
   "/complete",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await GetCompleteChallenge(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.get(
   "/:id",
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await GetChallenge(Number(req.params.id));
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.post(
   "/",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await PostChallenge(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.put(
   "/:id",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await PutChallenge(Number(req.params.id), req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.delete(
   "/:id",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await DeleteChallenge(Number(req.params.id));
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.post(
   "/participate/:id",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await JoinChallenge(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.post(
   "/complete/:id",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await MakeChallengeComplete(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 challengeRouter.post(
   "/certification/:id",
   AuthJWT,
-  async (req: express.Request, res: express.Response) => {
+  wrap(async (req: express.Request, res: express.Response) => {
     const { status, result } = await WriteCertificationArticle(req);
     res.status(status);
     res.send(result);
-  },
+  }),
 );
 
 export default challengeRouter;
